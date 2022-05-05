de622896ee607424501bf2f5585865e494694ade
ignaciobfp <ignaciobfp@gmail.com>
2022-05-05 17:15:16 +0200

    Code to set environment variables, added dependency proj.db

--- ShpFileWriter.h
+++ ShpFileWriter.h
@@ -8,6 +8,8 @@
 #include <gdal.h>
 #include <ogrsf_frmts.h>
 #include<thread>
+#include<cstdlib>       // setenv()
+#include<filesystem>   
 using namespace std;
 
 std::string shp_path;
@@ -18,6 +20,51 @@ OGRLayer* poLayer;
 OGRSpatialReference* poSRS;
 std::vector<std::string> headers;
 std::vector<OGRFieldType> fieldTypes;
+
+inline bool SetPermanentEnvironmentVariable(LPCTSTR value, LPCTSTR data)
+{
+	HKEY hKey;
+	LPCTSTR keyPath = TEXT("System\\CurrentControlSet\\Control\\Session Manager\\Environment");
+	LSTATUS lOpenStatus = RegOpenKeyEx(HKEY_LOCAL_MACHINE, keyPath, 0, KEY_ALL_ACCESS, &hKey);
+	if (lOpenStatus == ERROR_SUCCESS)
+	{
+		//Set RegKey
+		LSTATUS lSetStatus = RegSetValueEx(hKey, value, 0, REG_SZ, (LPBYTE)data, lstrlen(data)*sizeof(char16_t) + 1);
+		RegCloseKey(hKey);
+
+		//Force update of changed environment variables
+		if (lSetStatus == ERROR_SUCCESS)
+		{
+			//SendMessageTimeout(HWND_BROADCAST, WM_SETTINGCHANGE, 0, (LPARAM)"Environment", SMTO_BLOCK, 100, NULL);
+			SendMessageTimeoutW(HWND_BROADCAST, WM_SETTINGCHANGE, 0, (LPARAM)L"Environment", SMTO_ABORTIFHUNG, 5000, NULL);
+			return true;
+		}
+	}
+
+	return false;
+}
+
+inline void updateEnvironmentVariablesNow() {
+	SendMessageTimeoutW(HWND_BROADCAST, WM_SETTINGCHANGE, 0, (LPARAM)L"Environment", SMTO_ABORTIFHUNG, 5000, NULL);
+}
+
+// Wrapper to allow seeting environment variables in a portable manner, since windows uses _putenv(x), opposed to unix setenv(x,y,z)
+inline int setenv(const char* name, const char* value, int overwrite)
+{
+	int errcode = 0;
+	if (!overwrite) {
+		size_t envsize = 0;
+		errcode = getenv_s(&envsize, NULL, 0, name);
+		if (errcode || envsize) return errcode;
+	}
+	return _putenv_s(name, value);
+}
+
+inline std::filesystem::path getCurrentProcessDirectory() {
+	wchar_t buffer[MAX_PATH];
+	GetModuleFileName(NULL, buffer, sizeof(buffer));
+	return std::filesystem::path(buffer).parent_path();
+}
 
 inline void generateWKT(double x, double y, char* sWKT) {
 	//Example WKT string: POINT(-2.514557 42.444437)
